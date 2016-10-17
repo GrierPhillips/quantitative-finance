6067686a6516cd7274027f9297260bc21fcf41f0
Grier <GrierPhillips@gmail.com>
2016-10-17 10:10:40 -0700

    Started code for pulling in ticker symbols from s&p 500 index from wikipedia

---
+++ s_p500_ticker_table.py
@@ -0,0 +1,22 @@
+#!/Users/Doedy/anaconda2/bin/python
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+
+# Get wikipedia's list of S&P 500 equities table
+source = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
+r = requests.get(source)
+html = r.text
+soup = BeautifulSoup(html, 'html.parser')
+table = soup.find(class_='wikitable sortable')
+
+# Extract values from table
+records = pd.DataFrame(header=['Symbol', 'Name', 'Sector'])
+rows = table.findAll('tr')
+for row in rows:
+    cells = row.findAll('td')
+    if cells:
+        symbol = cells[0].string
+        name = cells[1].string
+        sector = cells[2].string
+        records.app
